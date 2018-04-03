6ca85a929d6bf7d8caedf766d3a4959200379bb7
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-04-03 10:57:20 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,33 @@
+// Init dependencies 
+var express = require('express'),
+    ipfilter = require('express-ipfilter').IpFilter;
+
+//var IpDeniedError = new Error(); //???
+// Blacklist the following IPs 
+var ips = ['127.0.0.1'];
+
+var app = express()
+ 
+// Create the server 
+app.use(ipfilter(ips, {mode: 'allow'})); // refuse connection on localhost
+//app.use(ipfilter(ips)); // allow everything apart 127.0.0.1
+
+console.log("Starting server on 3000 using ips")
+app.listen(3000).on('IpDeniedError', console.log);
+
+//IpDeniedError exception required
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, _next) {
+    console.log('Error handler', err);
+    if(err instanceof IpDeniedError){
+      res.status(401);
+    }else{
+      res.status(err.status || 500);
+    }
+
+    res.render('error', {
+      message: 'You shall not pass',
+      error: err
+    });
+  });
+}
