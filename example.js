a9555ba60d8000208613ce02fbba2ca179cf188b
OneFirewall <37590172+onefirewall@users.noreply.github.com>
2018-04-03 11:42:43 +0200

    update format

--- example.js
+++ example.js
@@ -12,22 +12,22 @@ var app = express()
 app.use(ipfilter(ips, {mode: 'allow'})); // refuse connection on localhost
 //app.use(ipfilter(ips)); // allow everything apart 127.0.0.1
 
-console.log("Starting server on 3000 using ips")
+console.log("Starting server on 3000 using ips");
 app.listen(3000).on('IpDeniedError', console.log);
 
 //IpDeniedError exception required
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, _next) {
-    console.log('Error handler', err);
-    if(err instanceof IpDeniedError){
-      res.status(401);
-    }else{
-      res.status(err.status || 500);
-    }
+    app.use(function(err, req, res, _next) {
+        console.log('Error handler', err);
+        if (err instanceof IpDeniedError) {
+           res.status(401);
+        } else {
+          res.status(err.status || 500);
+        }
 
-    res.render('error', {
-      message: 'You shall not pass',
-      error: err
+        res.render('error', {
+            message: 'You shall not pass',
+            error: err
+        });
     });
-  });
 }
