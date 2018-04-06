2947347167f28f8a8f9a4f09e7aae6a73bf68acb
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-04-06 15:11:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var ModSecWinAPI = require('./example.js');
+var ModSecWinAPI = require('./ModSecWinAPI.js');
 var ipList = [ "192.168.1.2", "notAnIp", "2001:db8::1:0:0:1"];
 
 var winAPI = new ModSecWinAPI()
