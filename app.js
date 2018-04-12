bc7a2be083bbf33e3a5cb84db7772f4cee689e90
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-04-12 12:54:11 +0200

    changed API name

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var ModSecWinAPI = require('./ModSecWinAPI.js');
 var ipList = [ "192.168.1.2", "notAnIp", "2001:db8::1:0:0:1"];
 
-var winAPI = new ModSecWinAPI()
+var winAPI = new ModSecWinAPI();
 
-winAPI.async(ipList)
+winAPI.addIPs(ipList);
+//winAPI.deleteIPs(ipList);
