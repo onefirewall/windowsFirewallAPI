a7f19ed9aa1ceb4ea09553af0614b01dfba1b3d5
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-04-13 16:55:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var ipList = [ "192.168.1.2", "notAnIp", "2001:db8::1:0:0:1"];
 
 var winAPI = new ModSecWinAPI();
 
-winAPI.addIPs(ipList);
+//uncomment at will
+//winAPI.addIPs(ipList);
 //winAPI.deleteIPs(ipList);
